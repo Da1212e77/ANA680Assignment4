a8fb32df2d582a61099c1d421e58f28441007070
Da1212e77 <Darrellamio@yahoo.com>
2024-06-04 13:48:06 -0700

    update

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def predict():
     return render_template('index.html', predict=str(pred))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
+
