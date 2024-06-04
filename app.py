d369c247b3f409c7dcff53cfeb2faa8cfee1db23
Da1212e77 <Darrellamio@yahoo.com>
2024-06-04 12:39:51 -0700

    updated

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-from flask import Flask, request, jsonify, render_template
-import joblib
-import pandas as pd
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
 
 app = Flask(__name__)
 
-# Load the model
-model = joblib.load('file_iris.pkl')
+filename = 'file_iris.pkl'
+model = pickle.load(open(filename, 'rb')) 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    df = pd.DataFrame(data)
-    prediction = model.predict(df)
-    return jsonify(prediction.tolist())
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+    
+    Sepal_Length = request.form['sepal_length']
+    Sepal_Width = request.form['sepal_width']
+    Petal_Length = request.form['petal_length']
+    Petal_Width = request.form['petal_width']
+    
+    pred = model.predict(np.array([[Sepal_Length, Sepal_Width, Petal_Length, Petal_Width]]))
+    # print(pred)
+    return render_template('index.html', predict=str(pred))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
